2b6860884fce463f81a32a48541e60d66c59d5da
hkumar1403 <kumarhemant1403@gmail.com>
2026-02-15 02:17:32 +0530

    changed cookie logic

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -89,8 +89,8 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true in production
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
